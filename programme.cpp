4d7222978bc2fd85982e2c13aba5d0bea9b10f35
amaurygau <amaurygau@gmail.com>
2021-12-16 10:56:40 +0100

    commit branche

--- programme.cpp
+++ programme.cpp
@@ -1,11 +1,39 @@
-// faux fichier c++ pour dépot git 
-
 #include <iostream>
-#include <cstdlib>
+#include <set>
+#include<map>
 
 using namespace std;
 
-int main {
-    
- return 0;   
-}
+int main() {
+  set<int> myset ={1,2,3,4,5};
+
+  myset.insert(10);
+  myset.insert(42);
+  myset.erase(4);
+
+  myset.find(4);
+
+  for (auto el: myset){
+    cout << el <<'\n';
+  }
+
+
+  map <char, int> mymap= {{'a',1}, {'b',2},{'e', 5}};
+  mymap['d']= 4;
+  mymap.insert({'z',26});
+
+
+  for (auto el :mymap){
+    cout << el.first << " "<< el.second << endl ;
+    //first = key //second=value
+  }
+
+  //trouver un élément dans le dictionnaire
+  auto it = mymap.find('e');
+  if (it != mymap.end()){
+    cout << "Found: " << it->first << " " << it->second << endl;}
+  else 
+    cout << "Not Found" << endl;
+  
+
+} 
